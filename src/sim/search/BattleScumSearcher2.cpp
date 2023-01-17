5cc89388e457a20a71f21c9d29b68a2717157600
Davis Cook <daviscook447@gmail.com>
2023-01-17 06:30:40 -0500

    add scoring for transient fight

--- src/sim/search/BattleScumSearcher2.cpp
+++ src/sim/search/BattleScumSearcher2.cpp
@@ -466,6 +466,10 @@ double getMonsterHpScale(const Monster &m) {
         case MonsterId::ACID_SLIME_L:
         case MonsterId::SPIKE_SLIME_L:
             return 2.0;
+        // the amount of HP transient has is irrelevant to how well the player is doing since they never kill it
+        // score only purely based on remaining HP at the end of the rollout
+        case MonsterId::TRANSIENT:
+            return 0.0;
         default:
             return 1.0;
     }
@@ -511,7 +515,10 @@ double search::BattleScumSearcher2::evaluateEndState(const BattleContext &rootBc
     } else {
         double curHpTotal = getNonMinionMonsterCurHpTotal(bc);
         double maxHpTotal = getNonMinionMonsterMaxHpTotal(rootBc);
-        double hpRatio = curHpTotal / maxHpTotal;
+        double hpRatio = 0.0;
+        if (maxHpTotal != 0.0) {
+            curHpTotal / maxHpTotal;
+        }
 
         // produces losing scores in the range (-1.0, 0.0)
         return -hpRatio;
